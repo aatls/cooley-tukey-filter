ebca6ca37e1c8d5b308a31069d0626fabd912fe4
aatls <aatos.latva-somppi@helsinki.fi>
2024-10-12 12:41:27 +0300

    refactor UI

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,33 @@
 #define comp(a, b) std::complex<double>(a, b)
 #define d(msg) std::cout << msg << "\n" // for debugging
 
+void get_user_input(uint32_t sample_rate, uint32_t* freq1, uint32_t* freq2, double* gain1, double* gain2) {
+    std::cout << "Greetings!\n\n";
+    std::cout << "Please enter frequencies between 0 and " << sample_rate/2 << ".\nGain must be between 0 and 1\n\n";
+
+    bool correct_input = false;
+
+    while (!correct_input) {
+        std::cout << "Low freq & gain: ";
+        std::cin >> *freq1 >> *gain1;
+
+        std::cout << "High freq & gain: ";
+        std::cin >> *freq2 >> *gain2;
+
+        correct_input = *freq1 <= sample_rate/2 && *freq2 <= sample_rate/2 && *gain1 >= 0 && *gain1 <= 1 && *gain2 >= 0 && *gain2 <= 1;
+
+        if (!correct_input) {
+            std::cout << "\nInput out of range\n\n";
+        }
+    }
+
+    if (*freq2 < *freq1) {
+        std::cout << "Freqs are in the wrong order but ok";
+        std::swap(*freq1, *freq2);
+        std::swap(*gain1, *gain2);
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc <= 1) {
         std::cout << "Usage: cooley-tukey-filter infile [-o outfile]\n\tinfile & outfile must be of .wav format" << std::endl;
@@ -31,7 +58,7 @@ int main(int argc, char* argv[]) {
     }
 
     if (in_name.ends_with(".wav") == false || std::string_view(out_name).ends_with(".wav") == false) {
-        std:: cout << "audio input & output files must be .wav format" << std::endl;
+        std:: cout << "Audio input & output files must be .wav format" << std::endl;
         return 0;
     }
 
@@ -40,36 +67,14 @@ int main(int argc, char* argv[]) {
     audio.load(argv[1]);
 
     if (audio.getNumChannels() != 1) {
-        std::cout << "this program supports only mono audio" << std::endl;
+        std::cout << "This program supports only mono audio" << std::endl;
         return 0;
     }
 
-    // UI
-    std::cout << "Greetings!\n\n";
-    std::cout << "Please enter frequencies between 0 and " << audio.getSampleRate()/2 << ".\nGain must be between 0 and 1\n\n";
-
-    bool correct_input = false;
     uint32_t freq1, freq2;
     double gain1, gain2;
 
-    while (!correct_input) {
-        std::cout << "Low freq & gain: ";
-        std::cin >> freq1 >> gain1;
-
-        std::cout << "High freq & gain: ";
-        std::cin >> freq2 >> gain2;
-
-        correct_input = freq1 <= audio.getSampleRate()/2 && freq2 <= audio.getSampleRate()/2 && gain1 >= 0 && gain1 <= 1 && gain2 >= 0 && gain2 <= 1;
-
-        if (!correct_input) {
-            std::cout << "\nInput out of range\n\n";
-        }
-    }
-
-    if (freq2 < freq1) {
-        std::swap(freq1, freq2);
-        std::swap(gain1, gain2);
-    }
+    get_user_input(audio.getSampleRate(), &freq1, &freq2, &gain1, &gain2);
 
     // Transform the sample vector to it's discrete Fourier series
     auto fourier_series = ctf::radix2fft(audio.samples[0]);
@@ -78,7 +83,6 @@ int main(int argc, char* argv[]) {
 
     // Filter users freqs here
     ctf::band_cut(fourier_series, audio.getSampleRate(), freq1, freq2, gain1, gain2);
-    ctf::band_cut(fourier_series, audio.getSampleRate(), audio.getSampleRate() - freq2, audio.getSampleRate() - freq1, gain1, gain2);
 
     d("Filter applied..");
 
